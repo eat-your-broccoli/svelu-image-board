ac55bd6dd7fe356957cb53684c3dba5e4b306d77
Luca Göttle <l-goettle@gmx.de>
2022-06-03 11:25:14 +0200

    wip umzug setup

--- db/src/index.ts
+++ db/src/index.ts
@@ -1,1 +1,33 @@
-console.log('Hello');
+// index.js
+import { Sequelize } from 'sequelize';
+import { Umzug, SequelizeStorage } from 'umzug';
+
+const dbName = 'svelu'; // this name needs to be consistent with the terraform rds_db_name variable
+const username = 'root';
+const password = 'p4ssword1234!!!!';
+
+const dbConfig = {
+    port: 3306,
+    host: 'localhost'
+}
+
+const sequelize = new Sequelize(dbName, username, password, {
+    dialect: 'mysql',
+    dialectOptions: {
+      ...dbConfig,
+    }
+  })
+
+const umzug = new Umzug({
+  migrations: { glob: 'migrations/*.js' },
+  context: sequelize.getQueryInterface(),
+  storage: new SequelizeStorage({ sequelize }),
+  logger: console,
+});
+
+(async () => {
+  // Checks migrations and run them if they are not already applied. To keep
+  // track of the executed migrations, a table (and sequelize model) called SequelizeMeta
+  // will be automatically created (if it doesn't exist already) and parsed.
+  await umzug.up();
+})();
