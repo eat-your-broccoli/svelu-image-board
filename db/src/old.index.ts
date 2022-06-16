ffa91db224a0591f3a79692234c75053b415cf47
Luca Goettle <l-goettle@gmx.de>
2022-06-16 19:58:36 +0200

    delted not needed dir db

--- db/src/old.index.ts
+++
@@ -1,9 +0,0 @@
-// index.js
-import { umzug } from "./database";
-
-(async () => {
-  // Checks migrations and run them if they are not already applied. To keep
-  // track of the executed migrations, a table (and sequelize model) called SequelizeMeta
-  // will be automatically created (if it doesn't exist already) and parsed.
-  await umzug.up();
-})();
