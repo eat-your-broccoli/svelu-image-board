be8dbce344ec8af1747a0a927e43ee59a8ae926b
Luca Göttle <l-goettle@gmx.de>
2022-06-17 11:41:06 +0200

    wip

--- cloud-computing-app/src/App.js
+++ cloud-computing-app/src/App.js
@@ -1,55 +1,97 @@
 import React, { useState, useEffect } from 'react';
-// source upload: https://medium.com/geekculture/how-to-upload-and-preview-images-in-react-js-4e22a903f3db
-export default function UploadImages() {
-  const [images, setImages] = useState([]);
-  const [imageURLs, setImageURLs] = useState([]);
+import { CognitoUserPool  } from 'amazon-cognito-identity-js'
 
-  useEffect(()=> {
-    if (images.length < 1) return;
-    const newImageUrls = [];
-    images.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
-    setImageURLs(newImageUrls);
-  }, [images]);
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  function onImageChange(e) {
-    setImages([...e.target.files]);
-  }
+  const poolData = {
+    UserPoolId: 'eu-central-1_zPMNh6AcH',
+    ClientId: '5n8ig2i2kpr4ei3qo78svl1h4g'
+  };
+
+  const UserPool = new CognitoUserPool(poolData);
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    UserPool.signUp(email, password, [], null, (err, data) => {
+      if (err) console.error(err);
+      console.log(data);
+    });
+  };
+
   return (
-    <>
-         <label for="img_title">Image title:</label>
-            <input type="text" id="img_title" name="img_title" maxLength="20"/>
-            <br/>
-      Please select the Image you want to upload.
-      <br/>
-      <input type="file" multiple accept='image/*' onChange={onImageChange} />
-      <br/>
-      {imageURLs.map(imageSrc => <img src={imageSrc} width="200" height="auto"/>)}
-      <br/>
-      <label for="img_title">Image Description:</label>
-            <input type="text" id="img_title" name="img_title" maxLength="200" />
-            <br/>
-      <input type="submit" />
-      <br />
-     
-      <div className='Imagehoster'>
-           <h1>The Picture list</h1>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
 
-           <table>
-           <tr>
-           <th>Title 1           </th>
-           <th>    Title 2         </th>
-           <th>         Title 3    </th>
-           </tr>
-           <tr>
-           <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
-           <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
-           <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
-           </tr>
-         
-          </table>
-        </div>
+        <input
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
 
-     
-    </>
+        <button type='submit'>Signup</button>
+      </form>
+    </div>
   );
-}
+};
+
+
+// source upload: https://medium.com/geekculture/how-to-upload-and-preview-images-in-react-js-4e22a903f3db
+// export default function UploadImages() {
+//   const [images, setImages] = useState([]);
+//   const [imageURLs, setImageURLs] = useState([]);
+
+//   useEffect(()=> {
+//     if (images.length < 1) return;
+//     const newImageUrls = [];
+//     images.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
+//     setImageURLs(newImageUrls);
+//   }, [images]);
+
+//   function onImageChange(e) {
+//     setImages([...e.target.files]);
+//   }
+//   return (
+//     <>
+//          <label for="img_title">Image title:</label>
+//             <input type="text" id="img_title" name="img_title" maxLength="20"/>
+//             <br/>
+//       Please select the Image you want to upload.
+//       <br/>
+//       <input type="file" multiple accept='image/*' onChange={onImageChange} />
+//       <br/>
+//       {imageURLs.map(imageSrc => <img src={imageSrc} width="200" height="auto"/>)}
+//       <br/>
+//       <label for="img_title">Image Description:</label>
+//             <input type="text" id="img_title" name="img_title" maxLength="200" />
+//             <br/>
+//       <input type="submit" />
+//       <br />
+     
+//       <div className='Imagehoster'>
+//            <h1>The Picture list</h1>
+
+//            <table>
+//            <tr>
+//            <th>Title 1           </th>
+//            <th>    Title 2         </th>
+//            <th>         Title 3    </th>
+//            </tr>
+//            <tr>
+//            <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
+//            <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
+//            <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
+//            </tr>
+         
+//           </table>
+//         </div>
+
+     
+//     </>
+//   );
+// }
