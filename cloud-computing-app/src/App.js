ba9784bde38b09c3ab8c94e21f5a45ddd227743e
SvenTizian <92493713+SvenTizian@users.noreply.github.com>
2022-06-17 10:36:31 +0200

    gallery

---
+++ cloud-computing-app/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+// source upload: https://medium.com/geekculture/how-to-upload-and-preview-images-in-react-js-4e22a903f3db
+export default function UploadImages() {
+  const [images, setImages] = useState([]);
+  const [imageURLs, setImageURLs] = useState([]);
+
+  useEffect(()=> {
+    if (images.length < 1) return;
+    const newImageUrls = [];
+    images.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
+    setImageURLs(newImageUrls);
+  }, [images]);
+
+  function onImageChange(e) {
+    setImages([...e.target.files]);
+  }
+  return (
+    <>
+         <label for="img_title">Image title:</label>
+            <input type="text" id="img_title" name="img_title" maxLength="20"/>
+            <br/>
+      Please select the Image you want to upload.
+      <br/>
+      <input type="file" multiple accept='image/*' onChange={onImageChange} />
+      <br/>
+      {imageURLs.map(imageSrc => <img src={imageSrc} width="200" height="auto"/>)}
+      <br/>
+      <label for="img_title">Image Description:</label>
+            <input type="text" id="img_title" name="img_title" maxLength="200" />
+            <br/>
+      <input type="submit" />
+      <br />
+     
+      <div className='Imagehoster'>
+           <h1>The Picture list</h1>
+
+           <table>
+           <tr>
+           <th>Title 1           </th>
+           <th>    Title 2         </th>
+           <th>         Title 3    </th>
+           </tr>
+           <tr>
+           <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
+           <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
+           <td><img src={require('./img/testPicture.png')} height={200} weidth={200}/></td>
+           </tr>
+         
+          </table>
+        </div>
+
+     
+    </>
+  );
+}
